9d57f8b3eb87a263df293bb58dfbf3bd9795d055
tbalden <illespal@gmail.com>
2020-05-10 00:49:24 +0200

    uci: add initialization of UCI loading to proc cmdline
    (cherry picked from commit 049566529e95ad62de5cf6c760b1c683a890408b)

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -4,8 +4,32 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
+#if 1
+#include <linux/slab.h>
+#include <linux/spinlock.h>
+
+static bool done = false;
+
+static DEFINE_SPINLOCK(show_lock);
+static bool magisk = true;
+
+extern bool is_magisk(void);
+extern bool is_magisk_sync(void);
+extern void init_custom_fs(void);
+
+#endif
+
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
+#if 1
+	spin_lock(&show_lock);
+	if (done) {
+	} else {
+		magisk = is_magisk();
+		done = true;
+	}
+	spin_unlock(&show_lock);
+#endif
 	seq_puts(m, saved_command_line);
 	seq_putc(m, '\n');
 	return 0;
@@ -13,6 +37,9 @@ static int cmdline_proc_show(struct seq_file *m, void *v)
 
 static int __init proc_cmdline_init(void)
 {
+#if 1
+	init_custom_fs();
+#endif
 	proc_create_single("cmdline", 0, NULL, cmdline_proc_show);
 	return 0;
 }
