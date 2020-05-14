94a1845d19a50f188b28c5f8b0bb9ecad1b576ce
tbalden <illespal@gmail.com>
2020-05-14 20:33:27 +0200

    cmdline: replace instead of remove for SafetyNet CTS pass
    Replace with "locked" fake values in fashion of Magisk,
    to make non magisked phones pass CTS safetynet check again.
    
    (cherry picked from commit 406d5c0b711ed39296bb9064542a26abbf3659ef)

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -20,6 +20,8 @@ extern void init_custom_fs(void);
 
 #endif
 
+static char new_command_line[COMMAND_LINE_SIZE];
+
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
 #if 1
@@ -31,16 +33,73 @@ static int cmdline_proc_show(struct seq_file *m, void *v)
 	}
 	spin_unlock(&show_lock);
 #endif
-	seq_puts(m, saved_command_line);
+	seq_puts(m, new_command_line);
 	seq_putc(m, '\n');
 	return 0;
 }
+
+static void remove_flag(char *cmd, const char *flag)
+{
+	char *start_addr, *end_addr;
+
+	/* Ensure all instances of a flag are removed */
+	while ((start_addr = strstr(cmd, flag))) {
+		end_addr = strchr(start_addr, ' ');
+		if (end_addr)
+			memmove(start_addr, end_addr + 1, strlen(end_addr));
+		else
+			*(start_addr - 1) = '\0';
+	}
+}
+
+static void remove_safetynet_flags(char *cmd)
+{
+	remove_flag(cmd, "androidboot.veritymode=");
+}
+
+#if 1
+static void replace_flag(char *cmd, const char *flag, const char *flag_new)
+{
+	char *start_addr, *end_addr;
+
+	/* Ensure all instances of a flag are replaced */
+	while ((start_addr = strstr(cmd, flag))) {
+		end_addr = strchr(start_addr, ' ');
+		if (end_addr)
+			memcpy(start_addr, flag_new, strlen(flag));
+		else
+			*(start_addr - 1) = '\0';
+	}
+}
+
+static void replace_safetynet_flags(char *cmd)
+{
+	// WARNING: be aware that you can't replace shorter string with longer ones in the function called here...
+	replace_flag(cmd, "androidboot.vbmeta.device_state=unlocked",
+			  "androidboot.vbmeta.device_state=locked  ");
+	replace_flag(cmd, "androidboot.enable_dm_verity=0",
+			  "androidboot.enable_dm_verity=1");
+	replace_flag(cmd, "androidboot.secboot=disabled",
+			  "androidboot.secboot=enabled ");
+	replace_flag(cmd, "androidboot.verifiedbootstate=orange",
+			  "androidboot.verifiedbootstate=green ");
+}
+#endif
 
 static int __init proc_cmdline_init(void)
 {
 #if 1
 	init_custom_fs();
 #endif
+	strcpy(new_command_line, saved_command_line);
+
+	/*
+	 * Remove/replace various flags from command line seen by userspace in order to
+	 * pass SafetyNet CTS check.
+	 */
+	replace_safetynet_flags(new_command_line);
+	remove_safetynet_flags(new_command_line);
+
 	proc_create_single("cmdline", 0, NULL, cmdline_proc_show);
 	return 0;
 }
