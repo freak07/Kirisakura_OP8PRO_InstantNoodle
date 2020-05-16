0f7f4b38c4abf51c30c522d6476c9fc40ff4f018
tbalden <illespal@gmail.com>
2020-05-16 19:07:15 +0200

    cmdline: def conditional replace of SN
    (cherry picked from commit d890133d694168f5c805dd36b6720e0abf12ecfd)

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -3,6 +3,7 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <asm/setup.h>
 
 #if 1
 #include <linux/slab.h>
@@ -25,6 +26,7 @@ static char new_command_line[COMMAND_LINE_SIZE];
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
 #if 1
+	static int count = 0;
 	spin_lock(&show_lock);
 	if (done) {
 	} else {
@@ -32,12 +34,14 @@ static int cmdline_proc_show(struct seq_file *m, void *v)
 		done = true;
 	}
 	spin_unlock(&show_lock);
+	pr_info("%s cleanslate cmdline debugging. call number # %d\n",__func__,count++);
 #endif
 	seq_puts(m, new_command_line);
 	seq_putc(m, '\n');
 	return 0;
 }
 
+#ifdef REMOVE_SN
 static void remove_flag(char *cmd, const char *flag)
 {
 	char *start_addr, *end_addr;
@@ -56,8 +60,12 @@ static void remove_safetynet_flags(char *cmd)
 {
 	remove_flag(cmd, "androidboot.veritymode=");
 }
+#endif
 
-#if 1
+
+#ifdef REPLACE_SN
+static char *padding = "                ";
+
 static void replace_flag(char *cmd, const char *flag, const char *flag_new)
 {
 	char *start_addr, *end_addr;
@@ -65,8 +73,18 @@ static void replace_flag(char *cmd, const char *flag, const char *flag_new)
 	/* Ensure all instances of a flag are replaced */
 	while ((start_addr = strstr(cmd, flag))) {
 		end_addr = strchr(start_addr, ' ');
-		if (end_addr)
-			memcpy(start_addr, flag_new, strlen(flag));
+		if (end_addr) {
+			if (strlen(flag)<strlen(flag_new)) {
+				// xx yy=a zz
+				//    ^   ^
+				// xx yy=bb zz
+				int length_to_copy = strlen( start_addr + (strlen(flag) ) ) + 1; // +1 to copy trailing '/0'
+				int length_diff = strlen(flag_new)-strlen(flag);
+				memcpy(start_addr+(strlen(flag)+length_diff), start_addr+(strlen(flag)), length_to_copy);
+				memcpy(start_addr+(strlen(flag)), padding, length_diff);
+			}
+			memcpy(start_addr, flag_new, strlen(flag_new));
+		}
 		else
 			*(start_addr - 1) = '\0';
 	}
@@ -83,6 +101,11 @@ static void replace_safetynet_flags(char *cmd)
 			  "androidboot.secboot=enabled ");
 	replace_flag(cmd, "androidboot.verifiedbootstate=orange",
 			  "androidboot.verifiedbootstate=green ");
+	replace_flag(cmd, "androidboot.veritymode=logging",
+			  "androidboot.veritymode=enforcing");
+	replace_flag(cmd, "androidboot.veritymode=eio",
+			  "androidboot.veritymode=enforcing");
+
 }
 #endif
 
@@ -97,8 +120,12 @@ static int __init proc_cmdline_init(void)
 	 * Remove/replace various flags from command line seen by userspace in order to
 	 * pass SafetyNet CTS check.
 	 */
+#ifdef REPLACE_SN
 	replace_safetynet_flags(new_command_line);
+#endif
+#ifdef REMOVE_SN
 	remove_safetynet_flags(new_command_line);
+#endif
 
 	proc_create_single("cmdline", 0, NULL, cmdline_proc_show);
 	return 0;
