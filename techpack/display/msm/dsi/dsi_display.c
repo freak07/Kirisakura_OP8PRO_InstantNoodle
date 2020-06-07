5cee7bf7ae01cf1db266e2de15a4c2499aa323e7
tbalden <illespal@gmail.com>
2020-06-07 10:03:49 +0200

    dsi: backlight dimmer Instant setting added
    (cherry picked from commit 0456f6b31071c132aa11b6110a6b225f07b5339d)

--- techpack/display/msm/dsi/dsi_display.c
+++ techpack/display/msm/dsi/dsi_display.c
@@ -783,31 +783,12 @@ void dsi_rect_intersect(const struct dsi_rect *r1,
 }
 
 #ifdef CONFIG_UCI
-static int backlight_min = 3;
+static bool screen_is_on = false;
+static int backlight_min = 6;
 static bool backlight_dimmer = false;
 static u32 last_brightness;
 static bool first_brightness_set = false;
-
-static void uci_user_listener(void) {
-        {
-                bool change = false;
-                int on = backlight_dimmer?1:0;
-                int backlight_min_curr = backlight_min;
-
-                backlight_min = uci_get_user_property_int_mm("backlight_min", backlight_min, 2, 128);
-                on = !!uci_get_user_property_int_mm("backlight_dimmer", on, 0, 1);
-
-                if (on != backlight_dimmer || backlight_min_curr != backlight_min) change = true;
-
-                backlight_dimmer = on;
-
-                if (first_brightness_set && change) {
-//                        if (!(bl_g->bl_device->props.state & BL_CORE_FBBLANK)) {
-//                                dsi_backlight_update_status(bl_g->bl_device);
-//                        }
-                }
-        }
-}
+struct drm_connector *primary_connector = NULL;
 #endif
 
 extern int aod_layer_hide;
@@ -1055,7 +1036,8 @@ int dsi_display_set_backlight(struct drm_connector *connector,
 #ifdef CONFIG_UCI
 	DSI_INFO("bl_scale = %u, bl_scale_sv = %u, bl_lvl = %u\n",
 		bl_scale, bl_scale_sv, (u32)bl_temp);
-	if (backlight_dimmer) {
+	if (primary_display!=NULL && display == primary_display) {
+		if (backlight_dimmer) {
 		if (backlight_min < 45) {
 			if (bl_temp <= 46) {
 				int ratio = 47 - bl_temp; // 2 >= ratio >= 1
@@ -1066,6 +1048,9 @@ int dsi_display_set_backlight(struct drm_connector *connector,
 					backlight_min, bl_scale, bl_scale_sv, (u32)bl_temp);
 			}
 		}
+		}
+		last_brightness = bl_lvl;
+		first_brightness_set = true;
 	}
 #else
 	DSI_DEBUG("bl_scale = %u, bl_scale_sv = %u, bl_lvl = %u\n",
@@ -1114,6 +1099,29 @@ error:
 
 	return rc;
 }
+
+#ifdef CONFIG_UCI
+static void uci_user_listener(void) {
+        {
+                bool change = false;
+                int on = backlight_dimmer?1:0;
+                int backlight_min_curr = backlight_min;
+
+                backlight_min = uci_get_user_property_int_mm("backlight_min", backlight_min, 3, 45);
+                on = !!uci_get_user_property_int_mm("backlight_dimmer", on, 0, 1);
+
+                if (on != backlight_dimmer || backlight_min_curr != backlight_min) change = true;
+
+                backlight_dimmer = on;
+
+                if (first_brightness_set && change) {
+			if (primary_display != NULL && primary_connector != NULL && screen_is_on) {
+				dsi_display_set_backlight(primary_connector,primary_display, last_brightness);
+			}
+                }
+        }
+}
+#endif
 
 int dsi_display_cmd_engine_enable(struct dsi_display *display)
 {
@@ -2587,7 +2595,6 @@ static void _dsi_display_setup_misr(struct dsi_display *display)
 }
 
 extern int dsi_panel_set_aod_mode(struct dsi_panel *panel, int level);
-
 int dsi_display_set_power(struct drm_connector *connector,
 		int power_mode, void *disp)
 {
@@ -2609,9 +2616,11 @@ int dsi_display_set_power(struct drm_connector *connector,
 #ifdef CONFIG_UCI_NOTIFICATIONS_SCREEN_CALLBACKS
 	if (power_mode==SDE_MODE_DPMS_LP1 || power_mode==SDE_MODE_DPMS_OFF) { // 5 - fully off / 1 - AOD
 		ntf_screen_off();
+		screen_is_on = false;
 	} else if (power_mode==SDE_MODE_DPMS_ON) { // && bd->props.state!=2) { // 0 ON (state!= 0x02 it's a transient state while getting out of pocket, ON's 'state'
 				    // Without AOD props state can be 2 as well. Check user inputs instead
 		ntf_screen_on();
+		screen_is_on = true;
 	}
 #endif
 
@@ -9158,6 +9167,13 @@ wait_failure:
 		mutex_unlock(&display->display_lock);
 	}
 
+#ifdef CONFIG_UCI
+	if (primary_display!=NULL && primary_connector == NULL) {
+		if (primary_display == display) {
+			primary_connector = connector;
+		}
+	}
+#endif
 	SDE_ATRACE_END("dsi_display_pre_kickoff");
 	return rc;
 }
